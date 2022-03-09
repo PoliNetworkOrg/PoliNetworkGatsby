ef5f8243328d2a21141811cdd576c22fa854ff3e
Federico Grandi <fgrandi30@gmail.com>
2022-03-09 22:23:42 +0100

    chore: add Layout to all pages

--- src/pages/{LanguagesYaml.key}/index.tsx
+++ src/pages/{LanguagesYaml.key}/index.tsx
@@ -2,11 +2,10 @@ import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
-import Layout from "../../components/layout"
 import Seo from "../../components/seo"
 
 const IndexPage = (props: any) => (
-  <Layout>
+  <>
     <Seo title="Home" />
     <h1>Hi people. Language: {props.params.key}</h1>
     <p>{JSON.stringify(props, null, 2)}</p>
@@ -25,7 +24,7 @@ const IndexPage = (props: any) => (
       <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
       <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
-  </Layout>
+  </>
 )
 
 export default IndexPage
