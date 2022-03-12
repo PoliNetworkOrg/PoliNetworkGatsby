b9caf6aa52dccc799f8d99990dfbbc3b602532c7
Federico Grandi <fgrandi30@gmail.com>
2022-03-12 16:31:20 +0100

    chore: ensure consistency before and after hydration

---
+++ src/config/gatbsy-ssr.tsx
@@ -0,0 +1,4 @@
+import '../components/Layout/globalStyles.css'
+
+// This ensures that the layout is the same before and after React hydrates the page
+export { wrapPageElement } from './gatsby-browser'
