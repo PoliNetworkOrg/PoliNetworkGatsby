c1117e8eaec224cec04a4a418ef9f2c2360db340
Federico Grandi <fgrandi30@gmail.com>
2022-03-14 11:39:52 +0100

    chore: fix eslint plugin config

--- src/config/gatsby-config.ts
+++ src/config/gatsby-config.ts
@@ -50,7 +50,7 @@ export default {
       resolve: 'gatsby-plugin-eslint',
       options: {
         test: /\.js$|\.jsx$|\.ts$|\.tsx$/,
-        exclude: /(node_modules|.cache|public)/,
+        exclude: ['node_modules', '.cache', 'public'],
         stages: ['develop'],
         options: {
           emitWarning: true,
